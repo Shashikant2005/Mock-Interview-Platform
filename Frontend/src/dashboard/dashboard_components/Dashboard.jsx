6d9dff1d774a409087b9bd5a12726ab164f5d0ef
shashikant jadhav <jadhavshashikant677@gmail.com>
2025-05-27 16:27:08 +0530

    Small typo fix

--- Frontend/src/dashboard/dashboard_components/Dashboard.jsx
+++ Frontend/src/dashboard/dashboard_components/Dashboard.jsx
@@ -14,9 +14,6 @@ function Dashboard() {
     {/* previous interview list */}
 
     <HistoryInterviewList/>
-
-
-
 </div>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ Add this
+import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
+import router from './dashboard/router.jsx';
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file');
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+       <RouterProvider router={router} /> 
+    </ClerkProvider>
+  </React.StrictMode>
 );
